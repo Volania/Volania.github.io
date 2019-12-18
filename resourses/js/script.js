6dea6ec43817f416e065dc242ab4a0886743bcd6
Volania <tomilenkoks@ukr.net>
2019-12-18 18:18:27 +0200

    Jonas's bug fix

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -116,6 +116,34 @@ $(document).ready(function() {
             icon.removeClass('ion-close-round');
         }
     });
+
+    //This works basically like a media query for jQuery, where we can take different actions depending on the screen width
+
+    $(window).resize(function(){
+
+        var nav = $('.js--main-nav');
+        
+        var icon = $('.js--nav-icon i');
+        
+        if ($(window).width() > 767){
+        
+        nav.css("display", "block");
+        
+        icon.addClass('ion-close-round');
+        
+        icon.removeClass('ion-navicon-round');
+        
+        } else {
+        
+        nav.css("display", "none");
+        
+        icon.addClass('ion-navicon-round');
+        
+        icon.removeClass('ion-close-round');
+        
+        }
+    
+    });
 });
 
 
